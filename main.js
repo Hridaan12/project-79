c8853f72de9f491be9f31fbd6cbcf289446b65e0
Hridaan12 <71926511+Hridaan12@users.noreply.github.com>
2020-12-08 18:21:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+manu_list_erray = ["Chicken Tandoori Pizza", "Veg Supreame pizza, paneer tikkka pizza", "deluxe veg pizza", "veg Extravaganza pizza"]
+function getmenu(){
+    var htmldata;
+    htmldata = "<ol>"
+    manu_list_erray.sort()
+    for( i=0;i<manu_list_erray[i] + 
